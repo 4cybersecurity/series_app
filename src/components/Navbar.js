c24910726ae702f5778b6d9a79ccb9660c5e6b99
4cybersecurity <112129622+4cybersecurity@users.noreply.github.com>
2024-02-07 15:22:49 +0100

    homepage basics created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,31 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
-    return(
+    const currentUser = false
+    return (
         <div>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <Link to="/" navbar-brand text-white>
                         <h4>Series App</h4>
                     </Link>
+                    <div className="d-flex text-white
+                    align-items-center">
+                        {
+                            currentUser ? (
+                                    <>
+                                        <h5 className="mb-0 text-capitalize">{currentUser.displayName}</h5>
+                                        <button className="ms-2 btn btn-outline-light">Logout</button>
+                                    </>
+                                )
+                                : (
+                                    <>
+                                        <button className="ms-2 btn btn-outline-light">Login</button>
+                                        <button className="ms-2 btn btn-outline-light">Register</button>
+                                    </>
+                                )
+                        }
+                    </div>
                 </div>
             </nav>
         </div>
