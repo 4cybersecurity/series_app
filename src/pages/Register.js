964162429959b7ade7d19c6d26d4be040bff6f96
4cybersecurity <112129622+4cybersecurity@users.noreply.github.com>
2024-02-08 14:22:36 +0100

    register page form usecase added

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Register = () =>{
+    const [firstName, setFirstName] = useState()
+    const [lastName, setLastName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
     return(
         <div className="d-flex justify-content-center">
             <div className="form-image">
@@ -8,17 +13,62 @@ const Register = () =>{
             </div>
             <div className="register-form">
                 <h1 className="form-title display-3">Register</h1>
-                <form id="register">
+                <form id="register" onSubmit={null}>
                     <div className="mb-3">
                         <label htmlFor="firstName" className="form-label">
                             First Name
                         </label>
                         <input
+                            onChange={(e)=> setFirstName(e.target.value)}
                             type="text"
                             className="form-control"
                             placeholder="Enter first name.."
+                            required
                         />
                     </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="lastName" className="form-label">
+                            Last Name
+                        </label>
+                        <input
+                            onChange={(e)=>setLastName(e.target.value)}
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter last name.."
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">
+                            Email
+                        </label>
+                        <input
+                            onChange={(e)=>setEmail(e.target.value)}
+                            type="email"
+                            className="form-control"
+                            placeholder="Enter email.."
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">
+                            Password
+                        </label>
+                        <input
+                            onChange={(e)=>setPassword(e.target.value)}
+                            type="password"
+                            className="form-control"
+                            placeholder="Enter first password.."
+                            required
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary form-control">
+                        Register
+                    </button>
                 </form>
             </div>
         </div>
