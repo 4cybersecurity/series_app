4306e9d4ecbefeab60f23376335c960eecdd3c7e
4cybersecurity <112129622+4cybersecurity@users.noreply.github.com>
2024-02-08 14:07:13 +0100

    register page form created

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,8 +2,25 @@ import React from "react";
 
 const Register = () =>{
     return(
-        <div>
-            <h2>Register</h2>
+        <div className="d-flex justify-content-center">
+            <div className="form-image">
+                <img src={""} alt="sample-serie"/>
+            </div>
+            <div className="register-form">
+                <h1 className="form-title display-3">Register</h1>
+                <form id="register">
+                    <div className="mb-3">
+                        <label htmlFor="firstName" className="form-label">
+                            First Name
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter first name.."
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
