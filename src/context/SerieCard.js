d8b883ceecb54565d13f52a88e34fe6b59feb956
4cybersecurity <112129622+4cybersecurity@users.noreply.github.com>
2024-02-11 20:49:44 +0100

    worked on SerieCard.js

--- src/context/SerieCard.js
+++ src/context/SerieCard.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-const SerieCard = (props) => {
+const SerieCard = ({name, poster_path, overview, vote_average, id}) => {
     console.log(props)
-    return(
-        <div>
-            <h2>SerieCard</h2>
+    return (
+        <div className="serie">
+
+            <img
+                loading="lazy"
+                src={poster_path ? IMG_API + poster_path : defaultImage}
+                alt="serie-card"
+            />
+            <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
+                <h5>{name}</h5>
+            </div>
+            <div className="serie-over">
+                <h2>Overview</h2>
+                <p>{overview}</p>
+            </div>
         </div>
     );
 };
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import SerieCard from "../context/SerieCard";
-const API_KEY = process.env.REACT_APP_TVDB_KEY;
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=`;
 
 
 const Main = () => {
@@ -9,27 +12,37 @@ const Main = () => {
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        getSeries()
+        getSeries(FEATURED_API)
     }, []);
     const getSeries = (API) => {
         setLoading(true)
         axios.get(API)
             .then(res => setSeries(res.data.results))
             .catch((err) => console.log(err))
-            .finally(()=> setLoading(false))
+            .finally(() => setLoading(false))
     }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     if (searchTerm && currentUser) {
+    //         getMovies(SEARCH_API + searchTerm);
+    //     } else if (!currentUser) {
+    //         toastWarnNotify("Please log in to search a movie");
+    //     } else {
+    //         toastWarnNotify("Please enter a text");
+    //     }
+    // };
     return (
         <>
-            <h2>Main</h2>
-            {/*<div className="d-flex justify-content-center flex-wrap">*/}
-            {/*    {loading ? (*/}
-            {/*        <div className="spinner-border text-primary m-5" role="status">*/}
-            {/*            <span className="sr-only">Loading...</span>*/}
-            {/*        </div>*/}
-            {/*    ) : (*/}
-            {/*        series.map((serie) => <SerieCard key={{serie.id} {...serie}} />)*/}
-            {/*    )}*/}
-            {/*</div>*/}
+            <div className="d-flex justify-content-center flex-wrap">
+                {loading ? (
+                    <div className="spinner-border text-primary m-5" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                ) : (
+                    series.map((serie) => <SerieCard key={serie.id} {...serie}/>)
+                )}
+            </div>
         </>
     )
 }
