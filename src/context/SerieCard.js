b60bad646fa23eade5b4770db8b1c2271b26a20e
4cybersecurity <112129622+4cybersecurity@users.noreply.github.com>
2024-02-11 17:08:43 +0100

    seriecard component created

---
+++ src/context/SerieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SerieCard = (props) => {
+    console.log(props)
+    return(
+        <div>
+            <h2>SerieCard</h2>
+        </div>
+    );
+};
+
+export default SerieCard;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import SerieCard from "../context/SerieCard";
 const API_KEY = process.env.REACT_APP_TVDB_KEY;
 
 
@@ -20,6 +21,15 @@ const Main = () => {
     return (
         <>
             <h2>Main</h2>
+            {/*<div className="d-flex justify-content-center flex-wrap">*/}
+            {/*    {loading ? (*/}
+            {/*        <div className="spinner-border text-primary m-5" role="status">*/}
+            {/*            <span className="sr-only">Loading...</span>*/}
+            {/*        </div>*/}
+            {/*    ) : (*/}
+            {/*        series.map((serie) => <SerieCard key={{serie.id} {...serie}} />)*/}
+            {/*    )}*/}
+            {/*</div>*/}
         </>
     )
 }
