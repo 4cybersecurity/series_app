21a774ef9d62c3f0c95ba5aad53ef5a08d281f48
4cybersecurity <112129622+4cybersecurity@users.noreply.github.com>
2024-02-12 01:16:44 +0100

    private router created

--- src/pages/SerieDetail.js
+++ src/pages/SerieDetail.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {Link, useParams} from "react-router-dom";
 
 const SerieDetail = () => {
+    const [serieDetails, setSerieDetails] = useState("");
+    const {
+        name,
+        poster_path,
+        overview,
+        vote_average,
+        vote_count,
+    } = serieDetails;
+    const {id} = useParams();
     const API_KEY = process.env.REACT_APP_TMDB_KEY;
     const serieDetailBaseUrl = `https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}`;
     const baseImageUrl = "https://www.themoviedb.org/t/p/w1280/";
     const defaultImage = "https://www.themoviedb.org/t/p/w1280/4edFyasCrkH4MKs6H4mHqlrxA6b.jpg";
-    return(
-        <></>
-    )
+
+
+    useEffect(() => {
+        axios.get(serieDetailBaseUrl)
+            .then(res => setSerieDetails(res.data))
+            .catch(err => console.log(err))
+    }, []);
+    return (
+        <div className="container py-5">
+            <h1 className="text-center">{name}</h1>
+            <div className="card mb-3">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img
+                            src={poster_path ? baseImageUrl + poster_path : defaultImage}
+                            className="img-fluid rounded-start"
+                            alt="..."
+                        />
+                    </div>
+                    <div className="col-md-8 d-flex flex-column ">
+                        <div className="card-body">
+                            <h5 className="card-title">Overview</h5>
+                            <p className="card-text">{overview}</p>
+                        </div>
+                        <ul className="list-group ">
+                            <li className="list-group-item">{"Rate : " + vote_average}</li>
+                            <li className="list-group-item">
+                                {"Total Vote : " + vote_count}
+                            </li>
+                            <li className="list-group-item">
+                                <Link to={-1} className="card-link">Go Back</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>)
 }
 export default SerieDetail;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,15 +1,20 @@
-import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React, {useContext} from "react";
+import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Main from "../pages/Main";
 import SerieDetail from "../pages/SerieDetail";
+import {AuthContext} from "../context/AuthContext";
 
 
 
 
 const AppRouter = () => {
+    const {currentUser} = useContext(AuthContext)
+    function PrivateRouter(){
+        return currentUser ? <Outlet/> : <Navigate to="/login" replace/>
+    }
     return(
         <BrowserRouter>
             <Navbar/>
@@ -17,7 +22,9 @@ const AppRouter = () => {
                 <Route path="/" element={<Main/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
+                <Route path="/details/:id" element={<PrivateRouter/>}>
                 <Route path="/details/:id" element={<SerieDetail/>}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
